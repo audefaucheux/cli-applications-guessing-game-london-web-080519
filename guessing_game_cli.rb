704f3851344429a319097f9e5fa6ae8e61ecd237
Aude Faucheux <aude53@gmail.com>
2019-07-28 14:17:01 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   puts "Guess the number"
   input = gets.chomp
 
-  if number == input
+  if number + 1 == input
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
